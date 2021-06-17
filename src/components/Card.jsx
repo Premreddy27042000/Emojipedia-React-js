9bd81d896c80ede4407fdc90de7b86947281ccf9
Premreddy27042000 <62111608+Premreddy27042000@users.noreply.github.com>
2021-06-17 16:40:30 +0530

    Initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Card(props) {
+  return (
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label="Tense Biceps">
+          {props.emoji}
+        </span>
+        <span>
+          {/* Tense Biceps */}
+
+          {props.name}
+        </span>
+      </dt>
+      <dd>
+        {/* “You can do that!” or “I feel strong!” Arm with tense biceps. Also
+            used in connection with doing sports, e.g. at the gym. */}
+        {props.meaning}
+      </dd>
+    </div>
+  );
+}
+
+export default Card;
